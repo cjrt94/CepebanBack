8010e65cd0746f78428e83f1bbee3a2760d8576b
cjrt94 <=>
2018-12-09 13:41:02 -0500

    Fav Icon

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -10,9 +10,9 @@
     <link rel="stylesheet" href="{{asset('front/css/estilos.css')}}">
     <link rel="stylesheet" href="{{asset('front/css/font-awesome.min.css')}}">
 
-    <link rel="icon" type="image/png" sizes="32x32" href="http://www.cepeban.edu.pe/instituto//favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="http://www.cepeban.edu.pe/instituto//favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="http://www.cepeban.edu.pe/instituto//favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('front/imagenes/favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('front/imagenes/favicon-16x16.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('front/imagenes/favicon-96x96.png')}}">
 
 
     <script  src="https://code.jquery.com/jquery-3.3.1.min.js"
